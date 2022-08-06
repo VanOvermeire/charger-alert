674e244cc59bd8a87efe054d85e957b2d91304ee
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2022-08-06 14:50:39 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// TODO:
+//  Lambda 1: call to rest endpoint with location triangle -> save in dynamodb
+//  Lambda 2: on cron, check if anything in dynamo - if so, do call to url and see if charger is available -> if so, send email
+//  Components: lambda, cron, dynamo
+//  CDK with Typescript, Rust, Github actions
+
+fn main() {
+    println!("Hello, world!");
+}
