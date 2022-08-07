2a6aa4df6a555aa35f03ab1af80ed950027277dc
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2022-08-07 12:14:53 +0200

    Try with rustup instead (so we can cache it)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // TODO:
 //  Lambda 1: call to rest endpoint with location triangle -> save in dynamodb
-//  Lambda 2: on cron, check if anything in dynamo - if so, do call to url and see if charger is available -> if so, send email
+//  Lambda 2: on cron, check if anything in dynamo - if so, do call to url and see if charger is available -> if so, send email (and delete request in dynamo)
 //  Components: lambda, cron, dynamo
 //  optionally pass in a name or id of a charging point
 //  CDK with Typescript, Rust, Github actions
