7cf8856c443ec42884907459ac4b3337d66f430b
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2022-08-20 13:56:30 +0200

    Remove example code for check charger main

--- check_charger/src/main.rs
+++ check_charger/src/main.rs
@@ -17,15 +17,6 @@ async fn flow(_: LambdaEvent<Value>) -> Result<Value, Error> {
     //  - and delete the entry
     // (if not we will try again later)
 
-    // test
-    let client = reqwest::blocking::Client::new();
-    let data = "departure%5Blat%5D=50.844837&departure%5Blng%5D=4.39695&NELat=50.84691587&NELng=4.4037956&SWLat=50.84283428&SWLng=4.39634442";
-    let res = client.post("https://nl.chargemap.com/json/charging/pools/get_from_areas")
-        .header("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
-        .body(data)
-        .send()?;
-    println!("{:?}", res);
-
     Ok(json!(
         { "message": "done" }
     ))
