13a228b17c4fec49c2d0acf24bbae5107fb482e4
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2022-08-21 18:48:30 +0200

    Split up flow function

--- check_charger/src/main.rs
+++ check_charger/src/main.rs
@@ -1,13 +1,14 @@
-mod adapters;
-
-use std::future::Future;
-use futures::stream::{self, StreamExt};
 use std::sync::Arc;
+
 use futures::future::join_all;
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_runtime::{Error, LambdaEvent, service_fn};
 use serde_json::{json, Value};
+
 use common::{build_db_client, ChargerLambdaConfig};
-use crate::adapters::{AdapterError, build_email_client, build_http_client, CoordinatesDatabase, EmailClient, HttpClient};
+
+use crate::adapters::{AdapterError, build_email_client, build_http_client, CoordinatesDatabase, DbId, EmailClient, HttpClient};
+
+mod adapters;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -25,27 +26,26 @@ async fn main() -> Result<(), Error> {
     })).await
 }
 
-async fn flow<T: CoordinatesDatabase>(config: Arc<ChargerLambdaConfig>, arc_client: Arc<T>, http_client: Arc<HttpClient>, email_client: Arc<EmailClient>) -> Result<Value, Error> {
-    for item in arc_client.as_ref().get(config.get_table().0.as_str()).await? {
-        let chargers = http_client.as_ref().get_chargers(item.ne_lat, item.ne_lon, item.sw_lat, item.sw_lon).await?;
-
-        let t: Vec<Result<(), AdapterError>> = join_all(chargers.iter()
+async fn flow<T: CoordinatesDatabase>(config: Arc<ChargerLambdaConfig>, db_client: Arc<T>, http_client: Arc<HttpClient>, email_client: Arc<EmailClient>) -> Result<Value, Error> {
+    for item in db_client.get(config.get_table().0.as_str()).await? {
+        let chargers = http_client.get_chargers(item.ne_lat, item.ne_lon, item.sw_lat, item.sw_lon).await?;
+        let email_and_db_results: Vec<Result<(), AdapterError>> = join_all(chargers.iter()
             .filter(|c| c.available_connectors > 0)
             .map(|c| async {
                 println!("Charger with id {} has available connectors!", c.id);
                 // TODO get email
-                match email_client.send("FAKE&FAKE.com").await {
-                    Ok(_) => {
-                        println!("Email was sent, now deleting item in db");
-                        arc_client.as_ref().delete(config.get_table().0.as_str(), &item.id).await
-                    },
-                    Err(e) => Err(e),
-                }
+                send_email_and_delete_item(&item.id, config.clone(), db_client.clone(), email_client.clone()).await
             })
             .collect::<Vec<_>>()).await;
+        let _ = email_and_db_results.into_iter().collect::<Result<Vec<()>, AdapterError>>()?;
     };
 
     Ok(json!(
         { "message": "done" }
     ))
+}
+
+async fn send_email_and_delete_item<T: CoordinatesDatabase>(id: &DbId, config: Arc<ChargerLambdaConfig>, db_client: Arc<T>, email_client: Arc<EmailClient>) -> Result<(), AdapterError> {
+    email_client.send("FAKE&FAKE.com").await?;
+    Ok(db_client.delete(config.get_table().0.as_str(), id).await?)
 }
