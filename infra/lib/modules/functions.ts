0a90c9d9b70e20e0546e61c36cbe372291d51d43
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2022-08-21 15:06:33 +0200

    SES send permissions for checker

--- infra/lib/modules/functions.ts
+++ infra/lib/modules/functions.ts
@@ -2,6 +2,7 @@ import {Construct} from "constructs";
 import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
 import {Duration} from "aws-cdk-lib";
 import {Table} from "aws-cdk-lib/aws-dynamodb";
+import {Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 
 const handler = 'some.handler';
 
@@ -31,6 +32,11 @@ export const createFunctions = (scope: Construct) => (table: Table, region: stri
             EMAIL: source_email,
         },
     });
+    chargeChecker.addToRolePolicy(new PolicyStatement({
+        actions: ['ses:SendEmail', 'SES:SendRawEmail'],
+        resources: ['*'],
+        effect: Effect.ALLOW,
+    }));
     table.grantReadWriteData(chargeChecker);
 
     return {
