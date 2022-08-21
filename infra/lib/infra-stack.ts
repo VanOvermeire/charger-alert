6801c4bfe7717899a380fc2afde4857f781548c4
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2022-08-21 14:14:23 +0200

    Step two: set a fallback region

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -4,12 +4,13 @@ import {createFunctions} from "./modules/functions";
 import {createHttpApi} from "./modules/api";
 import {createDatabase} from "./modules/database";
 import {createCron} from "./modules/cron";
+import {DEFAULT_AWS_REGION} from "./modules/constants";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const region = process.env.AWS_REGION || "fake-region";
+    const region = process.env.AWS_REGION || DEFAULT_AWS_REGION;
 
     const table = createDatabase(this);
     const { alertAdder, chargeChecker } = createFunctions(this)(table, region);
--- infra/lib/modules/constants.ts
+++ infra/lib/modules/constants.ts
@@ -1,1 +1,2 @@
+export const DEFAULT_AWS_REGION = 'eu-west-1';
 export const API_VERSION = '/v1';
